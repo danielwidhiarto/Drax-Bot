d3e6f72d49209cc0fba1f20363976f58aa562456
danielwidhiarto <danielwidhiarto@gmail.com>
2021-01-27 12:58:07 +0700

    sdasa

--- commands/youtube.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'youtube',
-    description: "sends owner youtube links",
-    execute(message, args){
-        message.channel.send(`https://youtube.com/danielwidhiarto`);
-        }
-    }
